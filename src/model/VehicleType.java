c3ab2138c550d85794d6758a193f9feeb28a447e
TimurGadzhiev <timur.gadzhiev97@gmail.com>
2026-03-16 20:53:49 +0300

    first commit

--- src/model/VehicleType.java
+++ src/model/VehicleType.java
@@ -1,15 +1,9 @@
 package model;
 
-import java.util.Arrays;
-
 public enum VehicleType {
     CAR,
     PLANE,
     SHIP,
     BICYCLE,
-    CHOPPER;
-    public static String names() {
-        return Arrays.toString(values());
-        // Возвращает: "[CAR, PLANE, SHIP, BICYCLE, CHOPPER]"
-    }
+    CHOPPER
 }
